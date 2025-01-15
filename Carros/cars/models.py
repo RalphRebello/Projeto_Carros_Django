44518ccfba4b5b6a1f154fecae250d4b318b1194
ralph <ralph.rebello8@gmail.com>
2025-01-14 22:05:27 -0300

    Update frontend

--- Carros/cars/models.py
+++ Carros/cars/models.py
@@ -19,8 +19,6 @@ class Car(models.Model):
     value = models.FloatField(blank=True, null=True)
     photo = models.ImageField(upload_to='cars/', blank=True, null=True)
 
-
-
     def __str__(self):
         return self.model
     
--- Carros/cars/views.py
+++ Carros/cars/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
+from cars.models import Car
 
 
 # Create your views here.
 def cars_view(request):
+
+    search = request.GET.get('search')
+
+    cars = Car.objects.all().order_by('model')
+    # cars = Car.objects.filter(brand__name='Chevrolet')
+    # cars = Car.objects.filter(model='Chevette Tubarão')
+    # cars = Car.objects.filter(model__contains='Chevette')
+    if search:
+        cars = Car.objects.filter(model__icontains=search).order_by('model')
+
     return render(
         request, 
         'cars.html', 
-        {'cars': {'model': 'Astra 2.0'}}
+        {'cars': cars }
     )
