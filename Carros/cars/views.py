d92978bba6e1e6e7bf5edfeb8a8b7852c0e7c020
ralph <ralph.rebello8@gmail.com>
2025-02-17 21:04:22 -0300

    Signals

--- Carros/cars/views.py
+++ Carros/cars/views.py
@@ -1,6 +1,8 @@
 from cars.models import Car
 from cars.forms import CarModelForm
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
 
@@ -19,21 +21,21 @@ class CarsListView(ListView):
             cars = Car.objects.filter(model__icontains=search)
         
         return cars
-            
 
-class NewCarCreateView(CreateView):
-    
+
+class CarDetailView(DetailView):
+    model = Car
+    template_name = "car_detail.html"            
+
+@method_decorator(login_required(login_url='login'), name='dispatch')
+class NewCarCreateView(CreateView):  
     model = Car
     form_class = CarModelForm
     template_name = 'new_car.html'
     success_url = '/cars/'
 
 
-class CarDetailView(DetailView):
-    model = Car
-    template_name = "car_detail.html"
-
-
+@method_decorator(login_required(login_url='login'), name='dispatch')
 class CarUpdateView(UpdateView):
     model = Car
     form_class = CarModelForm
@@ -43,6 +45,7 @@ class CarUpdateView(UpdateView):
         return reverse_lazy('car_detail', kwargs={'pk': self.object.pk})
 
 
+@method_decorator(login_required(login_url='login'), name='dispatch')
 class CarDeleteView(DeleteView):
     model = Car
     template_name = 'car_delete.html'
